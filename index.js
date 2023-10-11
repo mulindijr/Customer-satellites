6311cd5b5af0989cd4ed65488953b39a34cf93b5
Brian Mulindi <mulindijrn@gmail.com>
2023-10-11 12:32:45 +0300

    Retrieve satellite data from the API

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
+    // Get references to the container and satellites container elements.
+    const container = document.querySelector('.container');
+    const satellitesContainer =  document.querySelector('.satellites-container');
+
+    // Fetch satellite data from the API.
+    fetch('https://isro.vercel.app/api/customer_satellites')
+    .then(res => res.json())
+    .then(data => {
+       
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
 });
