4c73d7a535872d5d4be072e2538356457dbbe52e
Brian Mulindi <mulindijrn@gmail.com>
2023-10-11 12:39:28 +0300

    Parse and display API response for customer satellites

--- index.js
+++ index.js
@@ -7,7 +7,39 @@ document.addEventListener('DOMContentLoaded', () => {
     fetch('https://isro.vercel.app/api/customer_satellites')
     .then(res => res.json())
     .then(data => {
-       
+        if (Array.isArray(data.customer_satellites)) {
+            data.customer_satellites.forEach(satellite => {
+                // Create a new div element for each satellite.
+                const div = document.createElement('div');
+                div.className = 'satellites-container';
+
+                // Create an element to display the satellite ID.
+                const satelliteId = document.createElement('li');
+                satelliteId.textContent = `${satellite.id}`;
+
+                // Create a container for satellite data.
+                const satelliteData = document.createElement('div');
+                satelliteData.className = 'satellite-data'
+
+                // Create elements to display specific satellite data.
+                const country = document.createElement('p');
+                country.textContent = `Country: ${satellite.country}`;
+
+                const launchDate = document.createElement('p');
+                launchDate.textContent = `Launch-Date: ${satellite.launch_date}`
+
+                const mass = document.createElement('p');
+                mass.textContent = `Mass: ${satellite.mass}`
+
+                const launcher = document.createElement('p');
+                launcher.textContent = `Launcher: ${satellite.launcher}`
+
+               
+
+            });
+        } else {
+            console.error('API response is not an array');
+        }
     })
     .catch(error => {
         console.error('Error fetching data:', error);
